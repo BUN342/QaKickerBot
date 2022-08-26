abe81ed747a69e2d60540c50ec0fe9c27b455640
Kirill <pimenov.k@inbox.ru>
2022-08-26 16:10:06 +0300

    fix start

--- test.py
+++ test.py
@@ -54,7 +54,7 @@ def handle_text(message):
     data = (message.from_user.first_name,)
     cursor.execute(sql, data)
     results = cursor.fetchall()
-    cursor.close()
+    
 
     # #user = User(message.from_user.first_name, 0)
     # if(message.from_user.first_name == "Yuriy"):
@@ -65,37 +65,36 @@ def handle_text(message):
         bot.send_message(chat_id, "Ты даже не зарегался\nНапиши /reg, рак")
     elif text == "/reg" or text == "/reg@qakickerratingbot":
         if not results:
-            cursor = conn.cursor()
+            
             sql = "INSERT INTO users (tg_name, scope) VALUES (%s, %s);"
             #data = (user.getName(), user.getScope())
             data = (message.from_user.first_name, 0)
 
             cursor.execute(sql, data)
             conn.commit()
-            cursor.close()
+            
             
             bot.send_message(chat_id, message.from_user.first_name + ', ты зарегался и сейчас у тебя TRAINEE I ранг.\nДумал все так просто будет?')
         else:
             bot.send_message(chat_id, message.from_user.first_name + ', ты уже зарегался')
     elif text == "/game4" or text == "/game4@qakickerratingbot":
         date = datetime.utcnow()-timedelta(minutes=POOL_TIME_FOR_GAME)
-        cursor = conn.cursor()
-        print(round(date.timestamp()))
+        
         sql = "SELECT last_upd FROM game_sessions WHERE last_upd > TO_TIMESTAMP(%s) ORDER BY last_upd DESC;"
         data = (round(date.timestamp()),)
 
         cursor.execute(sql, data)
         is_games = cursor.fetchone()
 
-        cursor.close()
+        
 
-        if(is_games is not None):
+        if(is_games is None):
             bot.send_message(chat_id, 'Игру уже кто-то начал.\nЗаверши предыдущую, прежде чем начать новую.')
             return
 
         bot.send_message(chat_id, 'Так, так, так.. Кто это тут у нас хочет начать игру?\nДавайте поможем %s собрать участников, пиши /me, если хочешь присоединиться к игре.' % message.from_user.first_name)
         
-        cursor = conn.cursor()
+        
         sqlINS = "INSERT INTO game_sessions (tg_name, win, chat_id, last_upd, game_id, side) VALUES (%s, %s, %s, %s, %s, %s);"
         
         data = (message.from_user.first_name, None, chat_id, datetime.utcnow(), message.from_user.id, True)
@@ -109,11 +108,11 @@ def handle_text(message):
         # cursor.execute(sqlUPD, data)
         
         conn.commit()
-        cursor.close()
+        
 
         
     # elif text == "/lose" or text == "/lose@qakickerratingbot":
-    #     cursor = conn.cursor()
+    #     
     #     sqlSEL = "SELECT scope FROM users WHERE tg_name = %s;"
     #     data = (message.from_user.first_name,)
     #     cursor.execute(sqlSEL, data)
@@ -132,43 +131,41 @@ def handle_text(message):
     #     data = (coins, message.from_user.first_name)
     #     cursor.execute(sqlUPD, data)
     #     conn.commit()
-    #     cursor.close()
+    #     
 
     #     bot.send_message(chat_id, 'Как так можно было? Отнимаю 25 очков')
     elif text == "/me" or text == "/me@qakickerratingbot":
         side = True
         date = datetime.utcnow()-timedelta(minutes=POOL_TIME_FOR_GAME)
 
-        cursor = conn.cursor()
-        print(round(date.timestamp()))
+        
+
         sql = "SELECT tg_name FROM game_sessions WHERE last_upd > TO_TIMESTAMP(%s) AND tg_name = %s ORDER BY last_upd DESC;"
         data = (round(date.timestamp()),message.from_user.first_name)
 
         cursor.execute(sql, data)
         is_player = cursor.fetchall()
-        cursor.close()
-
-        print(len(is_player))
+        
 
         if is_player is not None:
             bot.send_message(chat_id, '%s, ты уже записался на игру, жди начала' % message.from_user.first_name)
             return
         
 
-        cursor = conn.cursor()
+        
         sql="SELECT game_id, last_upd, side FROM game_sessions WHERE chat_id = %s AND last_upd > TO_TIMESTAMP(%s) ORDER BY last_upd DESC;"
         data=(chat_id, round(date.timestamp()))
 
         cursor.execute(sql, data)
         last_game = cursor.fetchall()
-        cursor.close()
+        
 
         if last_game is None:
             bot.send_message(chat_id, 'Данных нет!')
         elif(round(date.timestamp()) >= last_game[0][1].timestamp()):
             bot.send_message(chat_id, 'Игр нет!')
         else:
-            cursor = conn.cursor()
+            
             sql="INSERT INTO game_sessions (tg_name, win, chat_id, last_upd, game_id, side) VALUES (%s, %s, %s, %s, %s, %s);"        
             
             if side is True:
@@ -183,13 +180,13 @@ def handle_text(message):
             bot.send_message(chat_id, 'Все готовы?\nПишите /gamestart, чтобы начать игру.\nИли /gamestop, если хотите отменить игру')
 
             conn.commit()
-            cursor.close()
+            
     elif text == "/gamestart" or text == "/gamestart@qakickerratingbot":
             bot.send_message(chat_id, 'Игра началась!\n*дальнейший функционал будет готов в следующем релизе*')
     elif text == "/gamestop" or text == "/gamestop@qakickerratingbot":
             bot.send_message(chat_id, 'Чё, обоссались?\n*дальнейший функционал будет готов в следующем релизе*')
     elif text == "/mystat" or text == "/mystat@qakickerratingbot": 
-        cursor = conn.cursor()
+        
         sqlSEL = "SELECT scope FROM users WHERE tg_name = %s;"
         data = (message.from_user.first_name,)
         cursor.execute(sqlSEL, data)
@@ -198,7 +195,7 @@ def handle_text(message):
         sql = "SELECT name, max_scope FROM grades ORDER BY max_scope ASC"
         cursor.execute(sql)
         grades = cursor.fetchall()
-        cursor.close()
+        
 
         j = 0
         for i in grades:
@@ -215,7 +212,7 @@ def handle_text(message):
         bot.send_message(chat_id, message.from_user.first_name + ', твой ранг - %s. Давай поднажми, осталось совсем немного до нового ранга.' % rank)
             
     elif text == "/allstats" or text == "/allstats@qakickerratingbot":
-        cursor = conn.cursor()
+        
         sqlSEL = "SELECT tg_name, scope FROM users ORDER BY scope DESC;"
         cursor.execute(sqlSEL)
         scopes_with_names = cursor.fetchall()
@@ -223,7 +220,7 @@ def handle_text(message):
         sql = "SELECT name, max_scope FROM grades ORDER BY max_scope ASC"
         cursor.execute(sql)
         grades = cursor.fetchall()
-        cursor.close()
+        
 
         stat = "Рейтинг среди футболёров: \n"
         for n in scopes_with_names:
@@ -235,5 +232,5 @@ def handle_text(message):
 
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
-# cursor.close()
+# 
 # conn.close()
